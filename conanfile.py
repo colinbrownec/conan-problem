13215b9fa45354c74f2431e61237bc4336fdc0ed
memsharded <james@conan.io>
2016-08-06 01:20:19 +0200

    proposed solution

--- conanfile.py
+++ conanfile.py
@@ -5,29 +5,24 @@ import shutil
 class ConanProblemProject(ConanFile):
   name = 'ConanProblemProject'
   version = '1.0'
-
   settings = 'os', 'compiler', 'build_type', 'arch'
   generators = 'cmake'
-
   requires = 'Eigen3/3.2.8@bilke/stable'
-
   options = { 'shared': [True, False]}
   default_options = 'shared=False'
-
-  def source(self):
-    self.run("git clone https://github.com/colinbrownec/conan-problem.git")
+  exports = "src/*", "CMakeLists.txt"
 
   def build(self):
     cmake = CMake(self.settings)
+    # Small workaround, will fix it in conan 0.12
+    destination = self.package_folder if hasattr(self, "package_folder") else "."
     args = ' '.join([
-      '-DCMAKE_INSTALL_PREFIX=%s' % self.package_folder,
+      '-DCMAKE_INSTALL_PREFIX=%s' % destination,
       '-DBUILD_SHARED_LIBS=%s' % ('ON' if self.options.shared else 'OFF')
     ])
 
-    self.run('mkdir build')
-    #shutil.copy('conanbuildinfo.cmake', 'build')
-    self.run('cd build && cmake %s %s ../conan-problem' % (cmake.command_line, args))
-    self.run('cd build && cmake --build . --target install %s' % cmake.build_config)
+    self.run('cmake %s %s %s ' % (self.conanfile_directory, cmake.command_line, args))
+    self.run('cmake --build . --target install %s' % cmake.build_config)
 
   def package_info(self):
     self.cpp_info.libs = ['conan-problem-project']
